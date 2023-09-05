29b6e67f38ed0fe7c723a504c5003b87e915e32c
jf <fred26s@163.com>
2023-09-05 18:31:54 +0800

    初始化shadow页面

---
+++ src/components/toggle.vue
@@ -0,0 +1,26 @@
+<script setup>
+defineProps({
+  msgPre: {
+    type: String,
+  },
+  msgNext: {
+    type: String,
+  }
+})
+
+import { ref, computed } from 'vue'
+
+const status = ref('')
+
+</script>
+
+<template>
+ <div class="form-control">
+  <label class="label cursor-pointer">
+    <span class="label-text">{{ msgPre }}</span> 
+    <input type="checkbox" class="toggle" checked />
+  </label>
+</div>
+</template>
+
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,55 @@
 <script setup>
-// import TheWelcome from '../components/TheWelcome.vue'
+import { useFetch } from "../http/api"
+import { onBeforeMount, ref, computed, toValue, watchEffect, unref } from 'vue'
+import toggle from '../components/toggle.vue'
+
+let dataInfo = ref({})
+
+const fetchData = async (params) => {
+  // 默认使用 realtime，查看线上实时策略状态
+  const data = {
+    ...params
+  }
+  const { result, err } = await useFetch('/shadow/moon', {
+    method: 'post',
+    data
+  })
+  dataInfo.value = result.value;
+}
+
+const setData = async (params) => {
+  // 默认使用 realtime，查看线上实时策略状态
+  const data = {
+    ...params
+  }
+  const { result, err } = await useFetch('/summary/board', {
+    method: 'post',
+    data
+  })
+  dataInfo.value = result.value;
+}
+
+onBeforeMount(async () => {
+  fetchData()
+})
+
 </script>
 
 <template>
-  <main class="h-52 flex justify-center">
-    <progress class="progress w-56"></progress>
-  </main>
+  <div class="container mx-auto flex flex-col">
+    <div class="flex">
+      button
+    </div>
+    <div class="flex flex-col">
+      <div class="flex">
+        <toggle msgPre="Buy"></toggle>
+      </div>
+      <div class="flex">
+        toggle + input
+      </div>
+    </div>
+    <div class="flex">
+      submit
+    </div>
+  </div>
 </template>
